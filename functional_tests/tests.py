5190f573f75cb0962043c619f3788b3156087a80
jinl0ng <jinl0ng@jinl0ngdeMacBook-Pro.local>
2017-09-12 22:39:20 +0800

    functional_tests an app, use LiveServerTestCase
    q

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,15 +1,30 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+from contextlib import contextmanager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import staleness_of
+
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(1)
+        self.browser.implicitly_wait(2)
 
     def tearDown(self):
         self.browser.quit()
+
+
+    @contextmanager
+    def wait_for_page_load(self, timeout=30):
+        old_page = self.browser.find_element_by_tag_name('html')
+        yield WebDriverWait(self.browser, timeout).until(
+            staleness_of(old_page)
+        )
+
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
@@ -19,7 +34,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Edith has heard about a cool new online to-do lists
         #to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         #She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -40,19 +55,21 @@ class NewVisitorTest(unittest.TestCase):
         #When she hits enter, the page updates, and now the page lists
         #"1:Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        self.browser.implicitly_wait(2)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        with self.wait_for_page_load(timeout=10):
+            self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         #There is still text box inviting her to add another item. She
         #enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        self.browser.implicitly_wait(2)
 
         #The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        with self.wait_for_page_load(timeout=10):
+            self.check_for_row_in_list_table('1: Buy peacock feathers')
+            self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         #Edith wonders whether the site will remember her list. Then she sees
         #that the site has generated a unique URL for her -- there is some
@@ -61,6 +78,3 @@ class NewVisitorTest(unittest.TestCase):
 #She visits that URL - her to-do list is still there.
 
 #Satisfied, she goes back to sleep.
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
